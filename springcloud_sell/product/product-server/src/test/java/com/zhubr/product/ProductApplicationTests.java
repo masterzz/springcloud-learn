32abdb36454a8adc275581bb1709410ce6dbe07c
masterzz <545724476@qq.com>
2019-02-24 23:36:21 +0800

    fff

--- springcloud_sell/product/product-server/src/test/java/com/zhubr/product/ProductApplicationTests.java
+++ springcloud_sell/product/product-server/src/test/java/com/zhubr/product/ProductApplicationTests.java
@@ -11,6 +11,22 @@ public class ProductApplicationTests {
 
     @Test
     public void contextLoads() {
+        int i = 5;
+        switch (i) {
+            case 6:
+                System.out.println(1);
+                break;
+            case 2:
+                System.out.println(2);
+                break;
+            default:
+                System.out.println("default");
+//                如果不写break，后面的代码照样会运行，也就是说都是入口
+                break;
+            case 3:
+                System.out.println(5);
+                break;
+        }
     }
 
 }
